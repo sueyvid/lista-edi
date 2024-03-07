dc70498da75baae211b72fafd0dc5e21af07315c
labinfo2 <labinfo2@ectufrn.ect.ufrn.br>
2024-03-07 12:29:05 -0300

    linkedList add e get

--- list/list.go
+++ list/list.go
@@ -2,10 +2,10 @@ package list
 
 type IList interface {
 	AddEnd(value int)
-	// GetByIndex(index int) (int, error)
-	// AddByIndex(index, value int) error
-	// ModifyByIndex(index, value int) error
+	GetByIndex(index int) (int, error)
+	AddByIndex(index, value int) error
+	Set(index, value int) error
 	Size() int
-	// RemoveEnd() (int, error)
-	// RemoveByIndex(index int) (int, error)
+	RemoveEnd() (int, error)
+	RemoveByIndex(index int) (int, error)
 }
--- main_list/main.go
+++ main_list/main.go
@@ -6,13 +6,23 @@ import (
 )
 
 func main() {
+	// Criando a lista
 	size := 10
 	arraylist := list.ArrayList{}
 	arraylist.Init(size)
+
+	// Adicionando valores
 	for i := 0; i < size; i++ {
 		arraylist.AddEnd(i)
 	}
+
+	// Imprimindo
 	fmt.Print(arraylist)
+
+	// Teste AddEnd()
 	arraylist.AddEnd(5)
 	fmt.Print(arraylist)
+
+	// Teste GetByIndex()
+	arraylist.GetByIndex(2)
 }
